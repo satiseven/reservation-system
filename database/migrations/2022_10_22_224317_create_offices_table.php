d582347dd28c2c75bdb303f191dc1954beb99be3
satiseven <satiseven777@gmail.com>
2022-10-23 22:13:00 +0300

    first commit

---
+++ database/migrations/2022_10_22_224317_create_offices_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->index();
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('lat',11,8);
+            $table->decimal('lng',11,8);
+            $table->text('address_line_1');
+            $table->text('address_line_2')->nullable();
+            $table->tinyInteger('approval_status');
+            $table->boolean('hidden')->default(false);
+            $table->decimal('price_per_date',8,2);
+            $table->decimal('monthly_discount',8,2);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offices');
+    }
+};
